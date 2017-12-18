c689d5e7eade48b9223c25e0561a26c35662e415
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-12-18 19:44:27 +0100

    standard --fix (ignore)

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,6 @@ import Grid from '../components/Grid'
 export default () =>
   <Layout>
     <div className='content'>
-      <Grid imageUrl={'https://www.telemark.no/var/ezflow_site/storage/images/media/images/folkehelse/nyhetssaker/aarets-frivillighetskommune-2017/205025-1-nor-NO/AArets-frivillighetskommune-2017_responsive_12.jpg'}/>
+      <Grid imageUrl={'https://www.telemark.no/var/ezflow_site/storage/images/media/images/folkehelse/nyhetssaker/aarets-frivillighetskommune-2017/205025-1-nor-NO/AArets-frivillighetskommune-2017_responsive_12.jpg'} />
     </div>
   </Layout>
